ad314b385b6ed9c1c244b2797e299fdef110e2b5
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-07-18 19:14:20 -0300

    Class 1 - add a basic route

--- app.ts
+++ app.ts
@@ -4,6 +4,10 @@ const port = 3000
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
